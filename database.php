5f364bb3d52c9f67bb11c12ff0640108dc979b73
Chase Hausman <chase@chaseh.net>
2019-05-20 00:12:46 -0700

    Update database.php

--- database.php
+++ database.php
@@ -6,7 +6,7 @@
  * Time: 9:22 PM
  */
 
-$GLOBALS['connection'] = new mysqli("127.0.0.1", "cst126", "GCUCst126!", "cst126");
+$GLOBALS['connection'] = new mysqli("localhost", "cst126", "GCUCst126!", "cst126");
 
 if(!$GLOBALS['connection']) {
     echo "Could not connect to database.";
